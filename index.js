31b5de0002af85aaf32cb33d9109ae9fdef351ef
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-06 15:59:54 -0400

    Update test file and start consolidateCart function

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+let cart = [
+  {"AVOCADO": {price: 3.0, clearance: true}},
+  {"AVOCADO": {price: 3.0, clearance: true}},
+  {"KALE": {price: 3.0, clearance: false}}
+]
+
+
 const consolidateCart = (cart) => {
-  // code here
+
+  let newCart = []
+  cart.forEach(item => {
+    let productName = Object.keys(item)
+    newCart.push(productName)
+  })
+  console.log(newCart)
 }
+
 
 const applyCoupons = (cart, coupons) => {
   // code here
@@ -13,3 +27,5 @@ const applyClearance = (cart) =>{
 const checkout = (cart, coupons) => {
   // code here
 }
+
+consolidateCart(cart)
